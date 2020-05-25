a7dde5c8d37fe4df342c4183c172a45cfbc620bc
amarmulyak <amarm@softserveinc.com>
2020-05-25 12:02:59 +0300

    HW01

---
+++ hw01/amarm/task1.py
@@ -0,0 +1,9 @@
+a = int(input("Type value for variable 'a': "))
+b = int(input("Type value for variable 'b': "))
+
+print(f'a = {a}, b = {b}')
+print(f'a + b = {a + b}')
+print(f'a - b = {a - b}')
+print(f'a * b = {a * b}')
+print(f'a / b = {a / b}')
+
